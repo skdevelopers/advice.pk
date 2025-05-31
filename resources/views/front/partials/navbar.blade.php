dfa27891050f1999fed265394f28d4896cd9b3d7
skdevelopers <skdevelopers@live.com>
2025-05-31 22:06:01 +0500

    Frontend fixing  by Salman

--- resources/views/front/partials/navbar.blade.php
+++ resources/views/front/partials/navbar.blade.php
@@ -4,14 +4,7 @@
             <img src="{{ asset('assets/front/images/logo-dark.png') }}" class="inline-block dark:hidden" alt="Logo">
             <img src="{{ asset('assets/front/images/logo-light.png') }}" class="hidden dark:inline-block" alt="Logo">
         </a>
-        <div class="flex items-center">
-            <a href="{{ route('login') }}" class="btn btn-icon bg-green-600 text-white !rounded-full mr-2">
-                <i class="mdi mdi-account"></i>
-            </a>
-            <a href="{{ route('register') }}" class="btn bg-green-600 text-white !rounded-full hidden sm:inline">
-                Signup
-            </a>
-        </div>
+
         <div class="flex items-center space-x-6">
             <ul class="navigation-menu flex space-x-4">
                 <li><a href="{{ url('/') }}">Home</a></li>
@@ -20,6 +13,14 @@
                 <li><a href="{{ url('/listings') }}">Listing</a></li>
                 <li><a href="{{ url('/contact') }}">Contact</a></li>
             </ul>
+        </div>
+        <div class="flex items-center">
+            <a href="{{ route('login') }}" class="btn btn-icon bg-green-600 text-white !rounded-full mr-2">
+                <i class="mdi mdi-account"></i>
+            </a>
+            <a href="{{ route('register') }}" class="btn bg-green-600 text-white !rounded-full hidden sm:inline">
+                Signup
+            </a>
         </div>
     </div>
 </nav>
